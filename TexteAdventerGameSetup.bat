0f366c22271241eecc93fdf71380037252f8d997
antnhed <antnhe.debebe@aait.edu.et>
2024-05-23 22:14:58 +0300

    more update

---
+++ TexteAdventerGameSetup.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Creating virtual environment if it does not exist...
+if not exist textadventureenv (
+    python -m venv textadventureenv
+)
+
+echo Activating virtual environment...
+call textadventureenv\Scripts\activate
+
+echo Checking necessary Python packages...
+
+REM List of required Python packages
+set packages=pygame colorama
+
+REM Check if each package is installed
+for %%i in (%packages%) do (
+    pip show %%i >nul 2>&1
+    if errorlevel 1 (
+        echo Installing %%i...
+        pip install %%i
+    ) else (
+        echo %%i is already installed.
+    )
+)
+
+echo Compiling Python files...
+python -m compileall .
+
+echo Starting the game...
+python main_game.py
