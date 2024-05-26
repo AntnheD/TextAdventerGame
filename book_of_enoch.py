165e4d3a4ed4a621a70ff2cd9fe846b5c9422755
antnhed <antnhe.debebe@aait.edu.et>
2024-05-26 05:16:28 +0300

    upate

--- book_of_enoch.py
+++ book_of_enoch.py
@@ -1,6 +1,10 @@
 class BookOfEnoch:
     def start(self, game):
-        print("You have embarked on a journey to discover the Book of Enoch.")
-        # Add the specific story elements and interactions here
-        print("After solving ancient puzzles, you discover the Book of Enoch.")
-        game.update_score(150)
+        print("You have embarked on a journey to find the the book of enoch.")
+        game.start_monk_quiz()
+        if game.game_state["monk_quiz_passed"]:
+            print("After facing numerous challenges, you find the book of enoch.")
+            game.update_score(100)
+        else:
+            print("You failed the monk's quiz and couldn't proceed.")
+            game.game_over()
